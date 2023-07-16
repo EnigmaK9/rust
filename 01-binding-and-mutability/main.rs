8c9d6aa5928f919bd63a624fc907110ff0d50bc9
Ignacio Padilla <enigmak9@protonmail.com>
2023-07-15 23:42:21 -0600

    updated repo

--- 01-binding-and-mutability/main.rs
+++ 01-binding-and-mutability/main.rs
@@ -1,7 +1,7 @@
 //fix the error below with the least amount of modification to the code
 fn main() {
     let x: i32 = 5; //Uninitialized but used
-    let y: i32;     //Uninitialized but also unused, only a warning!
+    let _y: i32;     //Uninitialized but also unused, only a warning!
     assert_eq!(x,5);
     println!("Success!");
 }
