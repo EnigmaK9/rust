3e61f20b4da70cee83e9922166fcee5d775cbbc2
Ignacio Padilla <enigmak9@protonmail.com>
2023-12-08 01:27:20 -0600

    added exercise 8

---
+++ 08-destructuring/target/debug/deps/nombre_del_binario-543d9ca2d2eef19b.d
@@ -0,0 +1,5 @@
+/home/enigma/repos/rust/08-destructuring/target/debug/deps/nombre_del_binario-543d9ca2d2eef19b: src/main.rs
+
+/home/enigma/repos/rust/08-destructuring/target/debug/deps/nombre_del_binario-543d9ca2d2eef19b.d: src/main.rs
+
+src/main.rs:
